528306e1bf9fd0708a640c4aa361d9c37715f862
Rogier Bosschaart <rogierbosschaart@gmail.com>
2025-05-22 17:33:32 +0200

    did a lotta stuff forgot to push

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,6 +6,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    @bookmarks = Bookmark.where(list_id: @list.id)
   end
 
   def new
@@ -13,6 +14,13 @@ class ListsController < ApplicationController
   end
 
   def create
-    
+    @list = List.new(list_params)
+    @list.save
+    redirect_to root_path
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: "lists#index"
-  resources :lists, except: [:index]
+
+  resources :lists, except: [:index] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
 end
