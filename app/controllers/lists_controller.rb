2014d6dff84f6264f379d92a465652ac0ffa85c5
Rogier Bosschaart <rogierbosschaart@gmail.com>
2025-05-22 12:56:35 +0200

    worked on my lists#index and lists#new and my ListsController

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,18 @@
 class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    
+  end
 end
